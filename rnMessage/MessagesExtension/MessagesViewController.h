51623303dbc7de5e1248764a6c98a8a13fed66dd
yongho.park@zenome.co.kr <yongho.park@zenome.co.kr>
2016-10-07 11:20:27 +0900

    added source code

---
+++ rnMessage/MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  MessagesExtension
+//
+//  Created by y on 2016. 10. 6..
+//  Copyright © 2016년 zenome. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
