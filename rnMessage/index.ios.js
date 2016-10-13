48ce89099bd975815c36b7997a966df9b6a4b3a9
yongho.park@zenome.co.kr <yongho.park@zenome.co.kr>
2016-10-13 10:57:19 +0900

    modified container style

--- rnMessage/index.ios.js
+++ rnMessage/index.ios.js
@@ -185,7 +185,11 @@ class VirtualVehicle extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
